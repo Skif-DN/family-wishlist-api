13952d99ef558429bb21385423cb52dadbf8c02d
sKIF-DN <a.svtsk@gmail.com>
2026-02-26 12:39:03 +0200

    test: add unit and controller tests for WishService and WishController

---
+++ src/test/java/com/skif/familywishlist/controller/WishControllerTest.java
@@ -0,0 +1,279 @@
+package com.skif.familywishlist.controller;
+
+import com.skif.familywishlist.domain.Person;
+import com.skif.familywishlist.domain.Wish;
+import com.skif.familywishlist.dto.wish.WishRequestDTO;
+import com.skif.familywishlist.service.WishService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.UUID;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import static org.mockito.Mockito.*;
+
+@ActiveProfiles("test")
+@WebMvcTest(WishController.class)
+@AutoConfigureMockMvc(addFilters = false)
+class WishControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private WishService wishService;
+
+    @Test
+    void createWish_shouldReturnCreatedWish() throws Exception {
+        UUID wishId = UUID.randomUUID();
+        UUID ownerId = UUID.randomUUID();
+
+        Wish wish = new Wish();
+        wish.setId(wishId);
+        wish.setTitle("Bike");
+        wish.setDescription("Mountain Bike");
+
+        Person owner = new Person();
+        owner.setId(ownerId);
+        wish.setOwner(owner);
+
+        when(wishService.addWish(any(UUID.class), anyString(), anyString(), anyString())).thenReturn(wish);
+
+        mockMvc.perform(post("/wishes")
+        .contentType(MediaType.APPLICATION_JSON)
+                .content("""
+                            {
+                                "ownerId": "%s",
+                                "pin": "1234",
+                                "title": "Bike",
+                                "description": "Mountain Bike"
+                            }
+                            """.formatted(ownerId)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.title").value("Bike"));
+    }
+
+    @Test
+    void createWish_shouldReturnForbidden_whenPinInvalid() throws Exception {
+        UUID ownerId = UUID.randomUUID();
+
+        when(wishService.addWish(any(UUID.class), anyString(), anyString(), anyString()))
+                .thenThrow(new SecurityException("Invalid Pin"));
+
+        mockMvc.perform(post("/wishes")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("""
+                            {
+                                "ownerId": "%s",
+                                "pin": "wrong-pin",
+                                "title": "Bike",
+                                "description": "Mountain Bike"
+                            }
+                            """.formatted(ownerId)))
+                .andDo(print())
+                .andExpect(status().isForbidden())
+                .andExpect(jsonPath("$.message").value("Invalid Pin"));
+
+        verify(wishService).addWish(any(), eq("wrong-pin"), any(), any());
+    }
+
+    @Test
+    void createWish_shouldReturnNotFound_whenOwnerDoesNotExist() throws Exception {
+        UUID ownerId = UUID.randomUUID();
+
+        when(wishService.addWish(any(UUID.class), anyString(), anyString(), anyString()))
+        .thenThrow(new IllegalArgumentException("Owner not found"));
+
+        mockMvc.perform(post("/wishes")
+        .contentType(MediaType.APPLICATION_JSON)
+        .content("""
+                            {
+                                "ownerId": "%s",
+                                "pin": "1234",
+                                "title": "Bike",
+                                "description": "Mountain Bike"
+                            }
+                            """.formatted(ownerId)))
+                .andDo(print())
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.message").value("Owner not found"));
+    }
+
+    @Test
+    void deleteWish_shouldDeleteWish() throws Exception {
+        UUID wishId = UUID.randomUUID();
+
+        doNothing().when(wishService).deleteWish(wishId, "1234");
+
+        mockMvc.perform(delete("/wishes/{wishId}", wishId)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("""
+                        {
+                            "pin": "1234"
+                        }
+                        """))
+                .andDo(print())
+                .andExpect(status().isNoContent());
+
+        verify(wishService).deleteWish(wishId, "1234");
+    }
+
+    @Test
+    void deleteWish_shouldReturnForbidden_whenPinInvalid() throws Exception {
+        UUID wishId = UUID.randomUUID();
+
+        doThrow(new SecurityException("Invalid Pin")).when(wishService).deleteWish(wishId, "1111");
+
+        mockMvc.perform(delete("/wishes/{wishId}", wishId)
+            .contentType(MediaType.APPLICATION_JSON)
+            .content("""
+                        {
+                            "pin": "1111"
+                        }   
+                       """))
+                .andDo(print())
+                .andExpect(status().isForbidden())
+                .andExpect(jsonPath("$.message").value("Invalid Pin"));
+    }
+
+    @Test
+    void deleteWish_shouldReturnNotFound_whenOwnerDoesNotExist() throws Exception {
+        UUID wishId = UUID.randomUUID();
+
+        doThrow(new IllegalArgumentException("Wish not found")).when(wishService).deleteWish(wishId, "1234");
+
+        mockMvc.perform(delete("/wishes/{wishId}", wishId)
+        .contentType(MediaType.APPLICATION_JSON)
+        .content("""
+                        {
+                            "pin": "1234"
+                        }
+                        """))
+                .andDo(print())
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.message").value("Wish not found"));
+    }
+
+    @Test
+    void updateWish_shouldReturnUpdatedWish() throws Exception {
+        UUID wishId = UUID.randomUUID();
+        UUID ownerId = UUID.randomUUID();
+
+        Person owner = new Person();
+        owner.setId(ownerId);
+
+        Wish updateWish = new Wish();
+        updateWish.setId(wishId);
+        updateWish.setOwner(owner);
+        updateWish.setTitle("Updated title");
+        updateWish.setDescription("Updated description");
+
+        when(wishService.updateWish(eq(wishId), any())).thenReturn(updateWish);
+
+        String requestJson = """
+            {
+                "ownerId": "%s",
+                "pin": "1234",
+                "title": "Updated title",
+                "description": "Updated description"
+            }
+            """.formatted(ownerId);
+
+        mockMvc.perform(put("/wishes/{wishId}", wishId)
+            .contentType(MediaType.APPLICATION_JSON)
+                .content(requestJson))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.title").value("Updated title"))
+                .andExpect(jsonPath("$.description").value("Updated description"));
+
+        verify(wishService).updateWish(eq(wishId), any());
+    }
+
+    @Test
+    void updateWish_shouldReturnForbidden_whenInvalidPin() throws Exception {
+        UUID wishId = UUID.randomUUID();
+        UUID ownerId = UUID.randomUUID();
+
+
+
+        when(wishService.updateWish(eq(wishId), any())).thenThrow(new SecurityException("Invalid Pin"));
+
+        String requestJson = """
+            {
+                "ownerId": "%s",
+                "pin": "1234",
+                "title": "Updated title",
+                "description": "Updated description"
+            }    
+        """.formatted(ownerId);
+
+        mockMvc.perform(put("/wishes/{wishId}", wishId)
+        .contentType(MediaType.APPLICATION_JSON)
+                .content(requestJson))
+                .andDo(print())
+                .andExpect(status().isForbidden())
+                .andExpect(jsonPath("$.message").value("Invalid Pin"));
+
+        verify(wishService).updateWish(eq(wishId), any());
+    }
+
+    @Test
+    void updateWish_shouldReturnNotFound_whenOwnerDoesNotExist() throws Exception {
+        UUID wishId = UUID.randomUUID();
+        UUID ownerId = UUID.randomUUID();
+
+        when(wishService.updateWish(eq(wishId), any())).thenThrow(new IllegalArgumentException("Wish not found"));
+
+        String requestJson = """
+            {
+                "ownerId": "%s",
+                "pin": "1234",
+                "title": "Updated title",
+                "description": "Updated description"
+            }    
+        """.formatted(ownerId);
+
+        mockMvc.perform(put("/wishes/{wishId}", wishId)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestJson))
+                .andDo(print())
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.message").value("Wish not found"));
+
+        verify(wishService).updateWish(eq(wishId), any());
+    }
+
+    @Test
+    void updateWish_shouldReturnBadRequest_whenTitleIsBlank()  throws Exception {
+        UUID wishId = UUID.randomUUID();
+        UUID ownerId = UUID.randomUUID();
+
+        String requestJson = """
+            {
+                "ownerId": "%s",
+                "pin": "1234",
+                "title": "",
+                "description": "Updated description"
+            }    
+        """.formatted(ownerId);
+
+        mockMvc.perform(put("/wishes/{wishId}", wishId)
+        .contentType(MediaType.APPLICATION_JSON)
+                .content(requestJson))
+                .andDo(print())
+                .andExpect(status().isBadRequest());
+
+        verifyNoInteractions(wishService);
+    }
+}
---
+++ src/test/java/com/skif/familywishlist/service/WishServiceTest.java
@@ -0,0 +1,292 @@
+package com.skif.familywishlist.service;
+
+import com.skif.familywishlist.domain.Person;
+import com.skif.familywishlist.domain.User;
+import com.skif.familywishlist.domain.Wish;
+import com.skif.familywishlist.dto.wish.WishRequestDTO;
+import com.skif.familywishlist.repositories.PersonRepository;
+import com.skif.familywishlist.repositories.WishRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@ExtendWith(MockitoExtension.class)
+class WishServiceTest {
+    @Mock
+    private WishRepository wishRepository;
+
+    @Mock
+    private PersonRepository personRepository;
+
+    @Mock
+    private UserService userService;
+
+    @Mock
+    private PersonService personService;
+
+    @Mock
+    private PasswordEncoder passwordEncoder;
+
+    @InjectMocks
+    private WishService wishService;
+
+    @Test
+    void addWish_shouldCreateWish_whenPinIsValid() {
+
+        UUID ownerId = UUID.randomUUID();
+        String pin = "1234";
+
+        Person person = Mockito.mock(Person.class);
+
+        when(personRepository.findById(ownerId))
+                .thenReturn(Optional.of(person));
+
+        when(person.checkPin(pin, passwordEncoder))
+                .thenReturn(true);
+
+        Wish result = wishService.addWish(ownerId, pin, "Bike", "Mountain bike");
+
+        assertEquals("Bike", result.getTitle());
+        assertEquals("Mountain bike", result.getDescription());
+
+        verify(wishRepository).save(any(Wish.class));
+    }
+
+    @Test
+    void addWish_shouldThrowException_whenPersonNotFound() {
+
+        UUID ownerId = UUID.randomUUID();
+        String pin = "1234";
+
+        when(personRepository.findById(ownerId))
+                .thenReturn(Optional.empty());
+
+        IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> wishService.addWish(ownerId, pin, "Bike", "Mountain bike")
+        );
+
+        assertEquals("Person not found", exception.getMessage());
+
+        verify(wishRepository, never()).save(any());
+    }
+
+    @Test
+    void addWish_shouldThrowSecurityException_whenPinInvalid(){
+        UUID ownerId = UUID.randomUUID();
+        String pin = "1234";
+
+        Person person = Mockito.mock(Person.class);
+
+        when(personRepository.findById(ownerId))
+        .thenReturn(Optional.of(person));
+
+        when(person.checkPin(pin, passwordEncoder))
+                .thenReturn(false);
+
+        SecurityException exception = assertThrows(
+                SecurityException.class,
+                () -> wishService.addWish(ownerId, pin, "Bike", "Mountain bike"));
+
+            assertEquals("Invalid PIN", exception.getMessage());
+
+            verify(person).checkPin(pin, passwordEncoder);
+            verifyNoInteractions(wishRepository);
+            verify(wishRepository, never()).save(any());
+    }
+
+    @Test
+    void markAsFulfilled_shouldThrowException_whenAlreadyFulfilled() {
+        UUID wishId = UUID.randomUUID();
+        String pin = "1234";
+
+        Wish wish = Mockito.mock(Wish.class);
+        Person person = Mockito.mock(Person.class);
+
+        when(wishRepository.findById(wishId))
+                .thenReturn(Optional.of(wish));
+
+        when(wish.getOwner()).thenReturn(person);
+
+        when(person.checkPin(pin, passwordEncoder))
+        .thenReturn(true);
+
+        when(wish.isFulfilled()).thenReturn(true);
+
+        IllegalStateException exception = assertThrows(
+                IllegalStateException.class,
+                () -> wishService.markAsFulfilled(wishId, pin));
+
+        assertEquals("Wish already fulfilled", exception.getMessage());
+
+        verify(wish, never()).markAsFulfilled();
+    }
+
+    @Test
+    void markAsFulfilled_shouldThrowException_whenAlreadyUnfulfilled() {
+        UUID wishId = UUID.randomUUID();
+        String pin = "1234";
+
+        Wish wish = Mockito.mock(Wish.class);
+        Person person = Mockito.mock(Person.class);
+
+        when(wishRepository.findById(wishId))
+                .thenReturn(Optional.of(wish));
+
+        when(wish.getOwner()).thenReturn(person);
+
+        when(person.checkPin(pin, passwordEncoder))
+                .thenReturn(true);
+
+        when(wish.isFulfilled()).thenReturn(false);
+
+        IllegalStateException exception = assertThrows(
+                IllegalStateException.class,
+                () -> wishService.markAsUnfulfilled(wishId, pin));
+
+        assertEquals("Wish already unfulfilled", exception.getMessage());
+
+        verify(wish, never()).markAsUnfulfilled();
+    }
+
+    @Test
+    void updateWish_shouldThrowSecurityException_whenNotOwner(){
+        UUID ownerId = UUID.randomUUID();
+        UUID wishId = UUID.randomUUID();
+
+        WishRequestDTO wishRequestDTO = new WishRequestDTO();
+        wishRequestDTO.setOwnerId(ownerId);
+        wishRequestDTO.setTitle("New title");
+        wishRequestDTO.setDescription("New description");
+
+        Person person = Mockito.mock(Person.class);
+        User ownerUser = Mockito.mock(User.class);
+        User currentUser = Mockito.mock(User.class);
+
+        when(personService.getPersonById(ownerId))
+                .thenReturn(person);
+
+        when(person.getUser()).thenReturn(ownerUser);
+        when(ownerUser.getId()).thenReturn(UUID.randomUUID());
+
+        when(userService.getCurrentUser()).thenReturn(currentUser);
+        when(currentUser.getId()).thenReturn(UUID.randomUUID());
+
+        SecurityException exception = assertThrows(
+                SecurityException.class,
+                () -> wishService.updateWish(wishId,  wishRequestDTO));
+
+        assertEquals("Access denied", exception.getMessage());
+
+        verify(wishRepository, never()).save(any());
+    }
+
+    @Test
+    void updateWish_shouldUpdateWish_whenOwnerMatches(){
+        UUID ownerId = UUID.randomUUID();
+        UUID wishId = UUID.randomUUID();
+        UUID userId = UUID.randomUUID();
+
+        WishRequestDTO wishRequestDTO = new WishRequestDTO();
+        wishRequestDTO.setOwnerId(ownerId);
+        wishRequestDTO.setTitle("New title");
+        wishRequestDTO.setDescription("New description");
+
+        Person person = Mockito.mock(Person.class);
+        User ownerUser = Mockito.mock(User.class);
+        User currentUser = Mockito.mock(User.class);
+
+        Wish wish = new Wish("Old title", "Old description", person);
+        wish.setId(wishId);
+
+        when(personService.getPersonById(ownerId))
+                .thenReturn(person);
+
+        when(person.getUser()).thenReturn(ownerUser);
+        when(ownerUser.getId()).thenReturn(userId);
+
+        when(userService.getCurrentUser()).thenReturn(currentUser);
+        when(currentUser.getId()).thenReturn(userId);
+
+        when(person.getWishes()).thenReturn(List.of(wish));
+        when(wishRepository.save(wish)).thenReturn(wish);
+
+        Wish updatedWish = wishService.updateWish(wishId, wishRequestDTO);
+
+        assertEquals("New title", updatedWish.getTitle());
+        assertEquals("New description", updatedWish.getDescription());
+
+        verify(wishRepository).save(wish);
+    }
+
+    @Test
+    void deleteWish_shouldDeleteWish(){
+        UUID wishId = UUID.randomUUID();
+
+        String pin = "1234";
+
+        Wish wish = Mockito.mock(Wish.class);
+        Person person = Mockito.mock(Person.class);
+
+        when(wishRepository.findById(wishId)).thenReturn(Optional.of(wish));
+        when(wish.getOwner()).thenReturn(person);
+        when(person.checkPin(pin, passwordEncoder)).thenReturn(true);
+
+        wishService.deleteWish(wishId, pin);
+
+        verify(person).removeWish(wish);
+        verify(wishRepository).delete(wish);
+    }
+
+    @Test
+    void deleteWish_shouldThrowException_whenWishNotFound(){
+        UUID wishId = UUID.randomUUID();
+        String pin = "1234";
+
+        when(wishRepository.findById(wishId)).thenReturn(Optional.empty());
+
+        IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> wishService.deleteWish(wishId, pin)
+        );
+        assertEquals("Wish not found", exception.getMessage());
+
+        verify(wishRepository,  never()).delete(any());
+    }
+
+    @Test
+    void deleteWish_shouldThrowSecurityException_whenPinInvalid(){
+        UUID wishId = UUID.randomUUID();
+        String pin = "1234";
+
+        Wish wish = Mockito.mock(Wish.class);
+        Person person = Mockito.mock(Person.class);
+
+        when(wishRepository.findById(wishId)).thenReturn(Optional.of(wish));
+        when(wish.getOwner()).thenReturn(person);
+        when(person.checkPin(pin, passwordEncoder)).thenReturn(false);
+
+        SecurityException exception = assertThrows(
+                SecurityException.class,
+                () -> wishService.deleteWish(wishId, pin)
+        );
+
+        assertEquals("Invalid PIN", exception.getMessage());
+
+        verify(wish).getOwner();
+        verify(person).checkPin(pin, passwordEncoder);
+        verify(wishRepository, never()).delete(any());
+    }
+}
